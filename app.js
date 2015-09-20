434759c262c3df868ae907780a6bca2e05cae2f0
Matt Varblow <matt@varblow.net>
2015-09-20 16:51:41 -0400

    Made chilly's face resize with the browser window.  Added static file support to make chilly show up when running locally.

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
-app.get('/', function(req, res){
+app.use(express.static(__dirname + '/public'));
+app.get('/', function (req, res) {
  res.sendFile(__dirname + '/index.html');
 });
 io.on('connection', function(socket){
